f0c0ed553ae08c5cbb92d4eeb2f0e8ba49fa79bd
Giovana Vitoria <128054374+giovanavbs@users.noreply.github.com>
2023-08-17 22:32:59 -0300

    Update ex4.java

--- app/src/main/java/com/example/exerciciopam/ex4.java
+++ app/src/main/java/com/example/exerciciopam/ex4.java
@@ -52,34 +52,6 @@ public class ex4 extends AppCompatActivity {
                     }
                 });
 
-      
-        if(entrada <= 24 && entrada >= 0) {
-            if (entrada <= 18) {
-                horario = entrada + 6;
-                tvresult.setText(+entrada);
-            }
-            if (entrada == 19) {
-                tvresult.setText("1 da manhã");
-            }
-            if (entrada == 20) {
-                tvresult.setText("2 da manhã");
-            }
-            if (entrada == 21) {
-                tvresult.setText("3 da manhã");
-            }
-            if (entrada == 22) {
-                tvresult.setText("3 da manhã");
-            }
-            if (entrada == 23) {
-                tvresult.setText("4 da manhã");
-            }
-            if (entrada == 24) {
-                tvresult.setText("5 da manhã");
-            }
-            else{
-                tvresult.setText("horario invalido, insira um valor entre 0-24h!");
-            }
-        }
 
         btnex2.setOnClickListener(new View.OnClickListener() {
             @Override
